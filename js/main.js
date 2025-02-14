a5aa9d2ce0b5a8b3b24ae3ba0507c67b81be69dc
ali <ali.mo.eltweel@gmail.com>
2025-02-14 13:40:26 +0200

    setup my project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// open & close cart
+
+var cart = document.querySelector(".cart")
+
+function OpenCart(){
+  cart.classList.add("active")
+}
+
+function CloseCart(){
+  cart.classList.remove("active")
+}
